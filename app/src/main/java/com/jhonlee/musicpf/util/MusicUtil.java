1e481e6dde0342e75047f1788b524fd446e993d5
JhoneLeedx <lijing930615@live.com>
2017-03-28 18:02:55 +0800

    ...

--- app/src/main/java/com/jhonlee/musicpf/util/MusicUtil.java
+++ app/src/main/java/com/jhonlee/musicpf/util/MusicUtil.java
@@ -13,23 +13,20 @@ import java.util.List;
 
 public class MusicUtil {
 
-    public static List<Lyric> getLyrics(String lyric){
-
+    public static List<Lyric> getLyrics(String lyric) {
 
         List<Lyric> lyrics = new ArrayList<>();
         String lyricy = lyric.replace("\n", "");
-        if (lyricy!=null){
+        if (lyricy != null) {
             String[] strings = lyricy.trim().split("\\[");
-            for (int i=0;i<strings.length;i++){
+            for (int i = 0; i < strings.length; i++) {
                 int index = strings[i].indexOf("]");
-                if(index!=-1){
-                    if (strings[i].substring(0,index).trim().startsWith("ti:")){
-                    }else if (strings[i].substring(0,index).trim().startsWith("ar:")){
-                    }else if (strings[i].substring(0,index).trim().startsWith("al:")){
-                    }else {
+                if (index != -1) {
+                    String str = strings[i].substring(0, index).trim();
+                    if (str.startsWith("0")) {
                         Lyric lyc = new Lyric(Parcel.obtain());
-                        lyc.setLyric(strings[i].substring(index+1).trim());
-                        lyc.setTime(lrcData(strings[i].substring(0,index).trim()));
+                        lyc.setLyric(strings[i].substring(index + 1).trim());
+                        lyc.setTime(lrcData(str));
                         lyrics.add(lyc);
                     }
                 }
@@ -38,6 +35,7 @@ public class MusicUtil {
         return lyrics;
 
     }
+
     private static int lrcData(String time) {
         time = time.replace(":", "#");
         time = time.replace(".", "#");
@@ -46,7 +44,7 @@ public class MusicUtil {
         int mtime = Integer.parseInt(mTime[0]);
         int stime = Integer.parseInt(mTime[1]);
         int mitime = Integer.parseInt(mTime[2]);
-        int ctime = (mtime*60+stime)*1000+mitime*10;
+        int ctime = (mtime * 60 + stime) * 1000 + mitime * 10;
         return ctime;
     }
 }
